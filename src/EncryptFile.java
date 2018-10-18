24a31176d1c25dba54f8d8d9364f47564f9e1a8a
asia105022002 <asia105022002@gmail.com>
2018-10-18 11:56:39 +0800

    update

---
+++ src/EncryptFile.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class EncryptFile {
+    private JPanel panel1;
+    private JFormattedTextField formattedTextField1;
+    private JButton button1;
+    private JTextField textField1;
+    private JButton button2;
+    private JProgressBar progressBar1;
+}
